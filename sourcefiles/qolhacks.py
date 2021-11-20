b90179e8184df34ea23ba7151b44572725b73b05
Psuedoarc <indecomposable@gmail.com>
2021-11-19 23:10:12 -0500

    Fixed minor error in write.  Added main function.

--- sourcefiles/qolhacks.py
+++ sourcefiles/qolhacks.py
@@ -2,6 +2,7 @@
 from ctrom import CTRom
 import randosettings as rset
 
+
 # Trick the game into thinking P1 has the SightScope equipped, for enemy health
 # to always be visible.
 def force_sightscope_on(ctrom: CTRom, settings: rset.Settings):
@@ -11,17 +12,23 @@ def force_sightscope_on(ctrom: CTRom, settings: rset.Settings):
         ctrom.rom_data.seek(0x0CF039)
         # Ignore the result of that comparison and evaluate to always true, by
         # overwriting the BEQ (branch-if-equal) to BRA (branch-always)
-        ctrom.rom_data.write(0x80)
+        ctrom.rom_data.write(bytes([0x80]))
+
 
 # After writing additional hacks, put them here. Based on the settings, they
 # will or will not modify the ROM.
 def attempt_all_qol_hacks(ctrom: CTRom, settings: rset.Settings):
     force_sightscope_on(ctrom, settings)
 
-# Testing
-if __name__ == "__main__":
+
+def main():
     ctrom = CTRom.from_file("test1.sfc")
     settings = rset.Settings.get_new_player_presets()
     attempt_all_qol_hacks(ctrom, settings)
+
+
+# Testing
+if __name__ == "__main__":
+    main()
 
 
