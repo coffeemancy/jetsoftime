c8869c59cf8766eacfe3bda848f10569dbda88dd
Psuedoarc <indecomposable@gmail.com>
2021-12-10 11:54:00 -0500

    Added fastpendant.py for lw fast pendant.

---
+++ sourcefiles/fastpendant.py
@@ -0,0 +1,123 @@
+from dataclasses import dataclass
+
+import freespace
+import ctenums
+import ctevent
+from ctrom import CTRom
+import randosettings as rset
+import randoconfig as cfg
+
+
+@dataclass
+class ObjectRef:
+    loc_id: int = 0
+    obj_id: int = 0
+    func_id: int = 0
+
+
+def apply_fast_pendant_lw(ctrom: CTRom,
+                          settings: rset.Settings):
+    if not (rset.GameFlags.LOST_WORLDS in settings.gameflags and
+            rset.GameFlags.FAST_PENDANT in settings.gameflags):
+        return
+
+    # The strategy is just to change every 'if pendant charged' to
+    # 'if has pendant' since there's no well-defined event to add a scene
+    # to charge the pendant.  Especially in chronosanity.
+    TID = ctenums.TreasureID
+    LocID = ctenums.LocID
+
+    sealed_tids = [
+        TID.PORRE_ELDER_SEALED_1, TID.PORRE_ELDER_SEALED_2,
+        TID.PORRE_MAYOR_SEALED_1, TID.PORRE_MAYOR_SEALED_2,
+        TID.HECKRAN_SEALED_1,  # No need for sealed_2 because same trigger
+        TID.GUARDIA_CASTLE_SEALED_600,
+        TID.GUARDIA_CASTLE_SEALED_1000,
+        TID.GUARDIA_FOREST_SEALED_600, TID.GUARDIA_FOREST_SEALED_1000,
+        TID.TRUCE_INN_SEALED_600, TID.TRUCE_INN_SEALED_1000
+    ]
+
+    # Just to get the TID -> script reference dict
+    base_config = cfg.RandoConfig()
+    obj_refs = []
+    for tid in sealed_tids:
+        treasure_obj = base_config.treasure_assign_dict[tid]
+        obj_refs.append(
+            ObjectRef(treasure_obj.location,
+                      treasure_obj.object_id,
+                      treasure_obj.function_id)
+        )
+
+    # Non-treasure references are:
+    #   1) Bangor sealed door
+    #   2) Trann sealed door
+    #   3) Arris sealed door
+    #   4) Pyramid
+
+    bangor_door = ObjectRef(LocID.BANGOR_DOME, 8, 1)
+    trann_door = ObjectRef(LocID.TRANN_DOME, 0x11, 1)
+    arris_door = ObjectRef(LocID.ARRIS_DOME_LOWER_COMMONS, 9, 1)
+    pyramid = ObjectRef(LocID.FOREST_RUINS, 0, 0)
+
+    obj_refs.extend([bangor_door, trann_door, arris_door, pyramid])
+
+    script_man = ctrom.script_manager
+
+    for obj_ref in obj_refs:
+        script = script_man.get_script(obj_ref.loc_id)
+        obj_id = obj_ref.obj_id
+        func_id = obj_ref.func_id
+
+        # print(f"{obj_ref.loc_id}, {obj_ref.obj_id}, {obj_ref.func_id}")
+
+        start = script.get_function_start(obj_id, func_id)
+        end = script.get_function_end(obj_id, func_id)
+
+        pos = start
+
+        while True:
+            pos, cmd = script.find_command([0x16], pos, end)
+
+            if pos is None:
+                raise SystemExit(
+                    f"Failed to find pendant check in {obj_ref.loc_id}"
+                )
+
+            # Make sure it's the right command.
+            # 1st arg: 0xF4 indicating checking memory 0x7F00F4
+            # 2nd arg: 0x40 indicating we're operating with value 0x40
+            # 3rd arg: 0x06 indicating we're testing set bits
+            # 4th arg: jump length will vary from command to command
+            if cmd.args[0:3] == [0xF4, 0x40, 0x06]:
+                break
+
+            pos += len(cmd)
+
+        jump_length = cmd.args[3]
+        new_if = ctevent.EC.generic_two_arg(0xC9, 0xD6, jump_length)
+
+        script.delete_commands(pos, 1)
+        script.insert_commands(new_if.to_bytearray(), pos)
+
+
+def test():
+    with open('./roms/lwtest.sfc', 'rb') as infile:
+        rom = bytearray(infile.read())
+
+    ctrom = CTRom(rom, True)
+    space_man = ctrom.rom_data.space_manager
+    FSW = freespace.FSWriteType
+    space_man.mark_block((0x4F8000, 0x5F0000), FSW.MARK_FREE)
+
+    settings = rset.Settings.get_lost_worlds_presets()
+    settings.gameflags |= rset.GameFlags.FAST_PENDANT
+
+    apply_fast_pendant_lw(ctrom, settings)
+    ctrom.write_all_scripts_to_rom()
+
+    with open('./roms/lwtest_out.sfc', 'wb') as outfile:
+        outfile.write(ctrom.rom_data.getvalue())
+
+
+if __name__ == '__main__':
+    test()
