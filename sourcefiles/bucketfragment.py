6d00a7215699dfb29e02c035129d0a50b415e211
Psuedoarc <indecomposable@gmail.com>
2021-12-15 13:24:08 -0500

    Added first draft of bucket fragment files. Removed unneeded pyc file.

---
+++ sourcefiles/bucketfragment.py
@@ -0,0 +1,173 @@
+import random
+
+from ctrom import CTRom
+import ctenums
+import ctevent
+import ctstrings
+import logicfactory
+import logictypes
+import randosettings as rset
+import randoconfig as cfg
+import xpscale
+
+
+def set_bucket_function(ctrom: CTRom, settings: rset.Settings):
+    script_man = ctrom.script_manager
+    eot_script = script_man.get_script(ctenums.LocID.END_OF_TIME)
+
+    new_eot_script = ctevent.Event.from_flux('./flux/bucket_eot.Flux')
+
+    num_fragments = settings.bucket_settings.needed_fragments
+
+    start = new_eot_script.get_function_start(0x09, 1)
+    end = new_eot_script.get_function_end(0x09, 1)
+
+    # Find the GetItemAmount command (0xD7) as the new start
+    start, _ = new_eot_script.find_command([0xD7], start, end)
+
+    # find the if item_count >= amt
+    pos, cmd = new_eot_script.find_command([0x12], start, end)
+    new_eot_script.data[pos+2] = num_fragments
+
+    # find the temp = amt
+    pos, cmd = new_eot_script.find_command([0x4F], start, end)
+    new_eot_script.data[pos+1] = num_fragments
+
+    # find the two textbox commands, and add their strings to the eot script
+    pos_text = start
+    for i in range(2):
+        pos_text, text_cmd = new_eot_script.find_command([0xBB, 0xC1, 0xC2],
+                                                         pos_text, end)
+        str_ind = text_cmd.args[0]
+        new_string = new_eot_script.strings[str_ind]
+
+        eot_script.strings.append(new_string)
+        new_string_ind = len(eot_script.strings)-1
+
+        new_eot_script.data[pos_text+1] = new_string_ind
+
+        pos_text += len(text_cmd)
+
+    eot_script.modified_strings = True
+    func = new_eot_script.get_function(0x09, 0x01)
+    eot_script.set_function(0x09, 0x01, func)
+
+    xpscale.double_xp(ctrom)
+
+
+def set_fragment_properties(ctrom: CTRom):
+
+    # Let's just set the name to 'Fragment'
+    frag_name = ctstrings.CTString.from_str('Fragment', False)
+
+    # first byte is used to put a little icon if needed.  For key items
+    # this gets set to FF.
+    frag_name = b'\xFF'+frag_name
+
+    item_name_st = 0x0C0B5E
+    item_name_len = 0xB
+
+    frag_name_st = item_name_st + ctenums.ItemID.BUCKETFRAG*item_name_len
+    ctrom.rom_data.seek(frag_name_st)
+    ctrom.rom_data.write(frag_name)
+
+
+def write_fragments_to_config(settings: rset.Settings,
+                              config: cfg.RandoConfig):
+
+    if rset.GameFlags.BUCKET_FRAGMENTS not in settings.gameflags:
+        return
+
+    # Working with Chronosanity locations instead of raw TreasureIDs because
+    #   1) We're only putting fragments in Chronosanity locations, and
+    #   2) We have to deal with the linked location/pyramid
+
+    # Grab the locations where key items have been placed.
+    key_items = config.key_item_locations
+
+    # Make a flags + chronosanity game config to get all assignable locations
+    orig_flags = settings.gameflags
+    settings.gameflags |= rset.GameFlags.CHRONOSANITY
+    game_config = logicfactory.getGameConfig(settings, config)
+    settings.gameflags = orig_flags
+
+    loc_groups = game_config.locationGroups
+
+    LocationGroup = logictypes.LocationGroup
+    Location = logictypes.Location
+    TID = ctenums.TreasureID
+
+    # These locations are not in Chronosanity but they can get fragments?
+    desertLocations = LocationGroup("Sunken Desert", 0, lambda x: True)
+    (
+        desertLocations
+        .addLocation(Location(TID.SUNKEN_DESERT_B1_NE))
+        .addLocation(Location(TID.SUNKEN_DESERT_B1_NW))
+        .addLocation(Location(TID.SUNKEN_DESERT_B1_SE))
+        .addLocation(Location(TID.SUNKEN_DESERT_B1_SW))
+        .addLocation(Location(TID.SUNKEN_DESERT_B2_CENTER))
+        .addLocation(Location(TID.SUNKEN_DESERT_B2_E))
+        .addLocation(Location(TID.SUNKEN_DESERT_B2_N))
+        .addLocation(Location(TID.SUNKEN_DESERT_B2_NW))
+        .addLocation(Location(TID.SUNKEN_DESERT_B2_SE))
+        .addLocation(Location(TID.SUNKEN_DESERT_B2_SW))
+    )
+
+    prisonLocations = LocationGroup("Guardia Prison", 0, lambda x: True)
+    (
+        prisonLocations
+        .addLocation(Location(TID.GUARDIA_JAIL_CELL))
+        .addLocation(Location(TID.GUARDIA_JAIL_FRITZ))
+        .addLocation(Location(TID.GUARDIA_JAIL_FRITZ_STORAGE))
+        .addLocation(Location(TID.GUARDIA_JAIL_HOLE_1))
+        .addLocation(Location(TID.GUARDIA_JAIL_HOLE_2))
+        .addLocation(Location(TID.GUARDIA_JAIL_OMNICRONE_1))
+        .addLocation(Location(TID.GUARDIA_JAIL_OMNICRONE_2))
+        .addLocation(Location(TID.GUARDIA_JAIL_OMNICRONE_3))
+        .addLocation(Location(TID.GUARDIA_JAIL_OMNICRONE_4))
+        .addLocation(Location(TID.GUARDIA_JAIL_OUTER_WALL))
+    )
+
+    loc_groups.append(desertLocations)
+    loc_groups.append(prisonLocations)
+
+    all_locs = [loc for group in loc_groups for loc in group.locations]
+
+    # This is dangerous, but it should be OK since names are all generated
+    # automatically.
+    # TODO: write an __eq__ for locations that checks if the underlying
+    #       TreasureIDs are equal.  For now, we check getName() equality.
+    key_loc_names = [loc.getName() for loc in key_items]
+
+    # for name in key_loc_names:
+    #     print(name)
+
+    # print('****')
+
+    avail_locs = [loc for loc in all_locs
+                  if loc.getName() not in key_loc_names]
+
+    # for x in avail_locs:
+    #     print(x.getName())
+
+    # print('****')
+    num_fragments = settings.bucket_settings.num_fragments
+    fragment_locs = random.sample(avail_locs, num_fragments)
+
+    for x in fragment_locs:
+        x.setKeyItem(ctenums.ItemID.BUCKETFRAG)
+        x.writeKeyItem(config)
+
+
+def test():
+    # ctrom = CTRom.from_file('./roms/ct.sfc')
+    settings = rset.Settings.get_race_presets()
+    settings.gameflags |= rset.GameFlags.CHRONOSANITY
+    config = cfg.RandoConfig()
+
+    write_fragments_to_config(settings, config)
+    pass
+
+
+if __name__ == '__main__':
+    test()
---
+++ sourcefiles/xpscale.py
@@ -0,0 +1,155 @@
+
+from byteops import to_little_endian as tle, to_rom_ptr
+from ctrom import CTRom
+from freespace import FSWriteType
+
+# This is what happens to XP when an enemy is defeated
+# $FD/ABD6 BF 00 5E CC LDA $CC5E00,x[$CC:5E85] <-- XP
+# $FD/ABDA 18          CLC
+# $FD/ABDB 6D 8C B2    ADC $B28C  [$7E:B28C]
+# $FD/ABDE 8D 8C B2    STA $B28C  <-- Battle total XP
+
+# Then at the end of battle
+# $C1/F93E AE 8C B2    LDX $B28C  [$7E:B28C]
+# $C1/F941 DA          PHX
+# $C1/F942 9C 11 B3    STZ $B311  [$7E:B311] 
+# stuff... I think all that matters is doing something to $B28C at this point
+# in the code.
+
+# $FD/ABE1 BF 02 5E CC LDA $CC5E02,x[$CC:5E87] <-- GP
+# $FD/ABEF BF 06 5E CC LDA $CC5E06,x[$CC:5E8B] <-- TP
+
+
+def double_xp(ctrom: CTRom, mem_addr: int = 0x7E287E):
+    # rt asm
+    '''
+    C2 20             REP #$20
+    AD 8C B2          LDA $B28C
+    0A                ASL
+    10 03             BPL 03
+    A9 FF 7F          LDA #$7FFF
+    8D 8C B2          STA $B28C
+    AA                TAX
+    E2 20             SEP #$20
+    9C 11 B3          STZ $B311
+    6B                RTL
+    '''
+
+    rt = bytearray.fromhex(
+        'C2 20'              # REP #$20
+        'AD 8C B2'           # LDA $B28C
+        '0A'                 # ASL
+        '10 03'              # BPL 03
+        'A9 FF 7F'           # LDA #$7FFF
+        '8D 8C B2'           # STA $B28C
+        'AA'                 # TAX
+        'AD DB B2'           # LDA $B2DB (Battle TP)
+        '0A'                 # ASL
+        '10 06'              # BPL 06
+        'A9 FF 7F'           # LDA #$7FFF
+        'EE DD B2'           # INC $B2DD <-- Orig does this, why?
+        '8D DB B2'           # STA $B28C
+        'E2 20'              # SEP #$20
+        '9C 11 B3'           # STZ $B311
+        '6B'                 # RTL
+    )
+
+    len_rt = len(rt)
+    len_to_rtl = len_rt-1
+
+    mem_bank = (mem_addr >> 16) << 16
+    if mem_bank != 0x7E0000:
+        raise SystemExit('Address of flag must be in bank 7E')
+
+    mem_offset = mem_addr % 0x10000
+    mem_offset_b = tle(mem_offset, 2)
+    # LDA ${mem_addr}
+    check = (
+        b'\xAD' + mem_offset_b +            # LDA ${mem_offset}
+        b'\xD0\x05' +                       # BNE $05 (get to the doubling)
+        b'\xAE\x8C\xB2' +                   # LDX #$B28C
+        b'\x80' + bytearray([len_to_rtl])   # BRA #${len_to_rtl} (to RTL)
+    )
+
+    rt = check + rt
+
+    fsrom = ctrom.rom_data
+    space_man = fsrom.space_manager
+    rt_addr = space_man.get_free_addr(len(rt))
+
+    jsl_bytes = b'\x22' + tle(to_rom_ptr(rt_addr), 3)
+    # the NOPs cover the last two bytes of the original STZ $B311
+    hook = jsl_bytes + bytearray.fromhex('DA EA EA')
+
+    fsrom.seek(0x01F93E)
+    fsrom.write(hook)
+
+    fsrom.seek(rt_addr)
+    fsrom.write(rt, FSWriteType.MARK_USED)
+
+
+def scale_xp(ctrom: CTRom, mem_addr: int, scale_factor: int):
+    
+    # ASM for integer scaling things
+    '''
+    AE 8C B2          LDX $B28C
+    86 28             STX $28
+    AE XX XX          LDX ${mem_addr}
+    86 2A             STX $2A
+    22 0B 90 C1       JSL $C1C90B
+    AC 2C             LDX $2C
+    8E 8C B2          STX $B28C
+    9C 11 B3          STZ $B311
+    6B                RTL
+    '''
+    rt = bytearray.fromhex(
+        'AE 8C B2'
+        '86 28'
+        'AE 7F 28'  # Using 0x7E287F which is by luck in SRAM!
+        '86 2A'
+        '20 0B C9'
+        'A6 2C'
+        '8E 8C B2'
+        '9C 11 B3'
+        '6B'
+    )
+    fsrom = ctrom.rom_data
+    space_man = fsrom.space_manager
+    rt_addr = 0x01FFDF # space_man.get_free_addr(len(rt))
+
+    # Replace the LDX, PHX (4 bytes) with the JSL (also 4 bytes)
+    # The following STZ $B311 is part of the routine above, but the PHA is
+    # not, so at the hook ($C1/F93E) we need
+    '''
+    22 XX XX XX       JSL ${rt addr}
+    DA                PHX
+    EA                NOP
+    EA                NOP
+    '''
+
+    jsl_bytes = b'\x22' + tle(to_rom_ptr(rt_addr), 3)
+    # the NOPs cover the last two bytes of the original STZ $B311
+    hook = jsl_bytes + bytearray.fromhex('DA EA EA')
+    
+    fsrom.seek(0x01F93E)
+    fsrom.write(hook)
+
+    fsrom.seek(rt_addr)
+    fsrom.write(rt, FSWriteType.MARK_USED)
+
+
+def test_xp_scale():
+
+    ctrom = CTRom.from_file('./roms/ct.sfc')
+    ctrom.rom_data.patch_ips_file('./patch.ips')
+
+    space_man = ctrom.rom_data.space_manager
+    space_man.mark_block((0x5F0000, 0x600000), FSWriteType.MARK_FREE)
+
+    double_xp(ctrom)
+
+    with open('./roms/xp_test_out.sfc', 'wb') as outfile:
+        outfile.write(ctrom.rom_data.getvalue())
+
+if __name__ == '__main__':
+    test_xp_scale()
