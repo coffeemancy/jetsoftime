f8706bab71f8e95264d144a564b3766e2321e47d
Pseudoarc <indecomposable@gmail.com>
2022-04-28 11:49:48 -0400

    Some work with randomizing accessories.

--- sourcefiles/itemrando.py
+++ sourcefiles/itemrando.py
@@ -1,11 +1,11 @@
 import math
 import random
+
 import ctenums
 import ctstrings
 
 import randoconfig as cfg
 import randosettings as rset
-
 
 def getRandomPrice():
     r1 = random.uniform(0, 1)
@@ -83,19 +83,65 @@ def randomize_healing(settings: rset.Settings, config: cfg.RandoConfig):
             ' Lapis-M'
         )
 
+# Note:  This is done after roboribbon swaps stat boosts.
+
+
+class _BoostID(ctenums.StrIntEnum):
+    NOTHING = 0x00
+    SPEED_1 = 0x01
+    HIT_2 = 0x02
+    POWER_2 = 0x03
+    STAMINA_2 = 0x04
+    MAGIC_2 = 0x05
+    MDEF_5 = 0x06
+    SPEED_3 = 0x07
+    HIT_10 = 0x08
+    POWER_6 = 0x09
+    MAGIC_6 = 0x0A
+    MDEF_10 = 0x0B
+    POWER_4 = 0x0C
+    SPEED_2 = 0x0D
+    MDEF_15 = 0x0E
+    STAMINA_6 = 0x0F
+    MAGIC_4 = 0x10
+    MDEF_12 = 0x11
+    MAG_MDEF_5 = 0x12
+    POWER_STAMINA_10 = 0x13
+    MDEF_5_DUP = 0x14
+    MDEF_9 = 0x15
+
+
 def randomize_weapon_armor_stats(settings: rset.Settings,
                                  config: cfg.RandoConfig):
-    pass
+    BID = _BoostID
+    low_boosts = (
+        BID.HIT_2, BID.STAMINA_2, BID.POWER_2, BID.MAGIC_2
+    )
 
+    mid_boosts = (
+        BID.SPEED_1, BID.MDEF_5, BID.POWER_4, BID.STAMINA_6, BID.MAGIC_4
+    )
+
+    good_boosts = (
+        BID.SPEED_2, BID.MDEF_9, BID.POWER_6, BID.MAGIC_6, BID.HIT_10
+    )
+
+    high_boosts = (
+        BID.SPEED_3, BID.MDEF_15, BID.POWER_STAMINA_10, BID.MAG_MDEF_5
+    )
+        
 
 # This doesn't do much!  Most accessories are going to stay as-is because
 # their name says what they do.
 def randomize_accessories(settings: rset.Settings,
                           config: cfg.RandoConfig):
     IID = ctenums.ItemID
+
+    # Randomize counter mode of rage/frenzy
     counter_accs = (IID.RAGE_BAND, IID.FRENZYBAND)
 
     for item_id in counter_accs:
         item = config.itemdb[item_id]
         normal_counter = random.choice((True, False))
-        item.stats.normal_counter_mode = normal_counter
+        item.stats.has_normal_counter_mode = normal_counter
+
