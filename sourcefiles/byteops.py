5a3be9b28512508781b9099cad29fb61f26a9814
Pseudoarc <indecomposable@gmail.com>
2022-08-02 18:04:58 -0400

    Add methods for getting/setting masked ranges.

--- sourcefiles/byteops.py
+++ sourcefiles/byteops.py
@@ -121,3 +121,81 @@ def change_ptrs(rom, ptr_list, start, offsets, num_bytes=3):
 # Yes it feels weird having a one line function, but it gets used frequently.
 def file_ptr_from_rom(rom, addr, num_bytes=3):
     return to_file_ptr(get_value_from_bytes(rom[addr:addr+num_bytes]))
+
+
+def get_minimal_shift(mask: int):
+    '''
+    Find the minimal N such that (mask/2^N % 1) != 0.
+    '''
+    
+    # Put the binary string in reverse, and chop off the '0b' from the
+    # start.
+    try:
+        return bin(mask)[:1:-1].index('1')
+    except ValueError as orig:  # Give a more reasonable ValueError
+        raise ValueError('Mask must be nonzero.') from orig
+
+
+def get_masked_range(data: bytes, start_idx: int, num_bytes: int,
+                     mask: int, byteorder: str = 'little') -> int:
+    '''
+    Return the bytes in range(start_idx, start_idx+num_bytes) with
+    mask applied.
+
+    More precisely, this
+    1) Reads the bytes in range(start_idx, start_idx+num_bytes) as in
+      integer with the given byteorder.
+    2) Applies the mask to the integer.  In some sense this means the 
+       mask is big-endian.  The mask must have only consecutive bytes set.
+    3) Returns the masked bits as an integer.
+
+    Example (from the DB):
+    Location Exit	03	01FF	2	Destination Location
+        To get this range, you read bytes 3, 4 as a little-endian and then
+        apply the mask 0x1FF.  The actual bytes read are Bytes 3.FF and 4.01.
+        get_masked_range(self, 3, 2, 0x1FF, 'little')
+    '''
+    val = int.from_bytes(data[start_idx:start_idx+num_bytes], byteorder)
+    val &= mask
+    val >>= get_minimal_shift(mask)
+    return val
+
+
+def set_masked_range(data: bytearray, start_idx: int, num_bytes: int,
+                     mask: int, val: int, byteorder: str = 'little'):
+    '''
+    Set the bytes in range(start_idx, start_idx+num_bytes) corresponding
+    to bits set in mask to val.  Dual to get_masked_range such that
+        set_masked_range(start, n, mask, val, byteorder)
+        new_val = get_masked_range(start, n, mask, byteorder)
+    will have new_val == val.
+
+    More precisely, this
+    1) Reads the bytes in range(start_idx, start_idx+num_bytes) as in
+       integer with the given byteorder.  Python treats this as big-endian.
+    2) Write val (as big-endian) into the bits ste by mask.
+    3) Write the bytes back into self.
+
+    Example (from the DB):
+    Location Exit	03	01FF	2	Destination Location
+        To set this range, you read bytes 3, 4 as a little-endian int, write
+        a big-endian value into the 0x1FF bits, and write the result as
+        little-endian bytes into bytes 3 and 4.
+        set_masked_range(self, 3, 2, 0x1FF, set_val, 'little')
+    '''
+    shift = get_minimal_shift(mask)
+    max_val = mask >> shift  # assuming contiguous mask
+
+    if not 0 <= val <= max_val:
+        raise ValueError(
+            f'Value must be in range({max_val+1:0{2*num_bytes}X})'
+        )
+
+    inv_mask = (1 << (num_bytes*8)) - mask - 1
+    cur_val = int.from_bytes(data[start_idx: start_idx+num_bytes],
+                             byteorder)
+    cur_val &= inv_mask
+    cur_val |= (val << shift)
+
+    data[start_idx:start_idx+num_bytes] = \
+        cur_val.to_bytes(num_bytes, byteorder)
