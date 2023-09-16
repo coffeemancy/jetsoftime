2629b2917b5d5adc94b05aca3dfa56969c5178b1
C <caltronnotlrac@gmail.com>
2023-09-16 18:39:54 -0400

    more jotjson

--- sourcefiles/tests/test_jotjson.py
+++ sourcefiles/tests/test_jotjson.py
@@ -10,6 +10,9 @@ def settings():
     GF = rset.GameFlags
     settings = rset.Settings()
     settings.gameflags = GF.FIX_GLITCH | GF.EPOCH_FAIL
+    settings.bucket_settings.objectives_win = True
+    settings.char_settings.choices[5] == [5]
+    settings.tab_settings.scheme = scheme = rset.TabRandoScheme.BINOMIAL
     return settings
 
 
@@ -19,6 +22,9 @@ def test_json_encode_settings(settings):
 
 
 def test_json_decode_settings(settings):
-    data = json.dumps({'settings': settings}, cls=jotjson.JOTJSONEncoder)
+    data = json.dumps({'configuration': {'test': 1}, 'settings': settings}, cls=jotjson.JOTJSONEncoder)
     decoded = json.loads(data, cls=jotjson.JOTJSONDecoder)
     assert decoded['settings'] == settings, 'Decoded settings do not match initial settings'
+
+    # for now, we strip configuration out
+    assert 'configuration' not in decoded, 'Unexpected configuration in decoded JSON'
